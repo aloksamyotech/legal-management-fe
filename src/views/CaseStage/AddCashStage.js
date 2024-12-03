b9694142d3c81c057ae8c79087e9aa0667f0bf82
unknown <rahul.malviya@samyotech.com>
2024-12-03 18:55:13 +0530

    Case Stage Component Added

---
+++ src/views/CaseStage/AddCashStage.js
@@ -0,0 +1,116 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import {
+  FormControl,
+  helperText,
+  FormHelperText,
+  MenuItem,
+  Select,
+  FormLabel,
+  Grid,
+  TextField
+} from '@mui/material';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Typography from '@mui/material/Typography';
+import ClearIcon from '@mui/icons-material/Clear';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import { toast } from 'react-toastify';
+import Palette from '../../ui-component/ThemePalette';
+import {  Box } from '@mui/system';
+
+const AddCaseStage= (props) => {
+  const { open, handleClose } = props;
+ 
+  // -----------  validationSchema
+  const validationSchema = yup.object({
+    Title: yup.string().required('Title is required'),
+  });
+
+  // -----------   initialValues
+  const initialValues ={
+    Title: '',
+  };
+
+  
+
+  // formik
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: async (values) => {
+      console.log('Case Stage Data', values);
+      formik.resetForm();
+      handleClose();
+      toast.success('Case Stage Add successfully');
+      
+    }
+  });
+
+  return (
+    <div>
+      <Dialog
+      fullWidth
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+        
+      >
+        <DialogTitle
+          id="scroll-dialog-title"
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between'
+           
+          }}
+        >
+          <Typography style={{fontWeight:'normal'}} variant="h3">Create Case Stage</Typography>
+          <Typography>
+            <ClearIcon onClick={handleClose} style={{ cursor: 'pointer' }} />
+          </Typography>
+        </DialogTitle>
+        <DialogContent  dividers>
+          <form>
+            <DialogContentText height={150} id="scroll-dialog-description" tabIndex={-1} >
+              <Grid container rowSpacing={3} columnSpacing={{ xs: 0, sm: 5, md: 4}}>
+                <Grid item xs={12} sm={12} md={12}>
+                <Box mb={1}>
+
+                  <FormLabel style={{color:"black"}}>Title</FormLabel>
+                </Box>
+                  <TextField
+                    id="Title"
+                    name="Title"
+                    type="text"
+                    size="small"
+                    placeholder='Enter Case Stage'
+                    fullWidth
+                    value={formik.values.Title}
+                    onChange={formik.handleChange}
+                    error={formik.touched.Title && Boolean(formik.errors.Title)}
+                    helperText={formik.touched.Title && formik.errors.Title}
+                  />
+                </Grid>
+                
+                
+              </Grid>
+            </DialogContentText>
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={formik.handleSubmit} variant="contained" color="primary" type="submit">
+            Create
+          </Button>
+          
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default AddCaseStage;
---
+++ src/views/CaseStage/CaseStageData.js
@@ -0,0 +1,21 @@
+const stageData = [
+    {
+      id: 1,
+      Title: 'Case Evaluation',
+      CreatedAt: '20/11/2024',
+      action: 'Edit'
+    },
+    {
+      id: 2,
+      Title: 'Discovery Stage',
+      CreatedAt: '20/11/2024',
+      action: 'Edit'
+    },
+    {
+      id: 3,
+      Title: 'Case Intake',
+      CreatedAt: '20/11/2024',
+      action: 'Edit'
+    }
+  ];
+  export default  stageData;
